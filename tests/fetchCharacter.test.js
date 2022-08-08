d072eb3ed08fc6dd0f6dafc721299905cc4cee9d
pedro reis rimoldi <pedroreisrimoldi@gmail.com>
2022-08-08 15:42:32 -0300

    test

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,14 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Nome igual', async () => {
+    const hero = await fetchCharacter('720');
+    expect(hero.name).toBe('Wonder Woman')
+    console.log(hero.name);
+  });
+  it('Falha', async () => {
+    const fail = await fetchCharacter();
+    expect(fail).toEqual(new Error('You must provide an url'))
+    console.log(fail);
+  });
 });
